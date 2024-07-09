0876dd372d5c097b7df34484690b80268b5ba7b2
HannaMarzouk <89429192+HannaMarzouk@users.noreply.github.com>
2024-07-09 05:13:20 +0300

    Initial

---
+++ static/js/sidebars.js
@@ -0,0 +1,15 @@
+/* global bootstrap: false */
+document.querySelectorAll(".nav-link").forEach((link) => {
+  if (link.href === window.location.href) {
+      link.classList.add("active");
+      link.setAttribute("aria-current", "page");
+  }
+});
+(() => {
+    'use strict'
+    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+      new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  })()
+  
